6e319e87fdaf5c033261a1d7bfc5b7bfc3e7f2b5
laulexa <laura.alexandra.roa@gmail.com>
2022-10-15 23:50:32 -0700

    adding nodes folder

---
+++ nodes.js
@@ -0,0 +1,5 @@
+// const homeBtn = document.querySelector('.item home-btn');
+// const blogBtn = document.querySelector('.blog-btn');
+// const ThingsToDoBtn = document.querySelector('.Things-to-do-btn');
+// const bestRestaurantsBtn = document.querySelector('.best-restaurants-btn');
+// const aboutMeBtn = document.querySelector('.about-me-button');
